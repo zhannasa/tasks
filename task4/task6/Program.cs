b34a5b2ce5c3f81aa812d5122a23ec348c5463bf
zhannas <zhannas@gmail.com>
2023-03-27 18:29:43 +0200

    task6

---
+++ task4/task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+string input = Console.ReadLine();
+int n =int.Parse(input);
+
+if (n % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное"); 
+}
