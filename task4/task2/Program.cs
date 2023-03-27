758b52e6b94f089ba69a7265266663a5ea7fd510
zhannas <zhannas@gmail.com>
2023-03-27 18:42:57 +0200

    task2

---
+++ task4/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 2;
+int b = 10;
+int max = 0;
+int min = 0;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
